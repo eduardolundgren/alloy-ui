d083a8e43a1bd3ae0f4a96035e1f9b5bb08ffc8e
brunofarache <bruno.farache@liferay.com>
2012-08-17 18:19:15 -0300

    AUI-674 - regenerate

--- build/aui-notify/aui-notify.js
+++ build/aui-notify/aui-notify.js
@@ -1,55 +1,44 @@
 AUI.add('aui-notify', function(A) {
-var BORDER = 'border',
+var ALERT = 'alert',
+    BORDER = 'border',
     BOUNDING_BOX = 'boundingBox',
     CONTENT_BOX = 'contentBox',
+    INFO = 'info',
     NOTICE = 'notice',
     NOTIFY_ITEM = 'notify-item',
     SHADOW = 'shadow',
     SHOW_TRANSITION = 'showTransition',
     TEXT = 'text',
-    TEXT_NODE = 'textNode',
+    TIMEOUT = 'timeout',
+    TITLE = 'title',
     TYPE = 'type',
 
     getCN = A.ClassNameManager.getClassName,
 
-    CSS_SHADOW = getCN(NOTIFY_ITEM, SHADOW),
-    CSS_TEXT = getCN(NOTIFY_ITEM, TEXT),
-    CSS_TYPE_NOTICE = getCN(NOTIFY_ITEM, NOTICE);
-    CSS_TYPE_NOTICE_BORDER = getCN(NOTIFY_ITEM, NOTICE, BORDER),
+    CSS_SHADOW = getCN(NOTIFY_ITEM, SHADOW);
 
-    TPL_TEXT = '<div class="' + CSS_TEXT + '"></div>';
-
-A.NotifyItem = A.Base.create(NOTIFY_ITEM, A.Widget, [A.WidgetChild, A.WidgetPosition, A.WidgetPositionAlign], {
+A.NotifyItem = A.Base.create(NOTIFY_ITEM, A.Widget, [A.WidgetAutohide, A.WidgetChild, A.WidgetPosition, A.WidgetPositionAlign, A.WidgetStdMod], {
     bindUI: function() {
         var instance = this;
 
         instance.after('render', instance._afterRender);
+        instance.after('visibleChange', instance._afterVisibleChange);
     },
 
     renderUI: function() {
-        var instance = this,
-            contentBox = instance.get(CONTENT_BOX),
-            textNode = instance.get(TEXT_NODE);
-
-        contentBox.append(textNode);
-    },
-
-    syncUI: function() {
         var instance = this,
             border = instance.get(BORDER),
             boundingBox = instance.get(BOUNDING_BOX),
             contentBox = instance.get(CONTENT_BOX),
             shadow = instance.get(SHADOW),
             showTransition = instance.get(SHOW_TRANSITION),
-            text = instance.get(TEXT),
-            textNode = instance.get(TEXT_NODE),
             type = instance.get(TYPE);
 
-        if (type == NOTICE) {
-            contentBox.addClass(CSS_TYPE_NOTICE);
+        if (type) {
+            contentBox.addClass(getCN(NOTIFY_ITEM, type));
 
             if (border) {
-                contentBox.addClass(CSS_TYPE_NOTICE_BORDER);
+                contentBox.addClass(getCN(NOTIFY_ITEM, type, BORDER));
             }
         }
 
@@ -60,30 +49,49 @@ A.NotifyItem = A.Base.create(NOTIFY_ITEM, A.Widget, [A.WidgetChild, A.WidgetPosi
         if (showTransition) {
             boundingBox.transition(showTransition);
         }
+    },
 
-        textNode.html(text);       
+    syncUI: function() {
+        var instance = this,
+			text = instance.get(TEXT),
+			title = instance.get(TITLE);
+
+		instance.setStdModContent(A.WidgetStdMod.HEADER, title);
+		instance.setStdModContent(A.WidgetStdMod.BODY, text);
     },
 
     _afterRender: function() {
         var instance = this,
-            timeout = instance.get('timeout');
+            timeout = instance.get(TIMEOUT);
 
         if (timeout > 0) {
             setTimeout(function() {
-                instance._hide();
+                instance.hide();
             }, timeout);
         }
     },
 
-    _hide: function() {
+    _afterVisibleChange: function(event) {
         var instance = this,
-            boundingBox = instance.get(BOUNDING_BOX);
+            boundingBox = instance.get(BOUNDING_BOX),
+            hideTransition = instance.get('hideTransition');
 
-        boundingBox.transition(instance.get('hideTransition'), function() {
+        if (event.newVal) {
+            return;
+        }
+
+        if (hideTransition) {
+            boundingBox.transition(hideTransition, function() {
+                var index = instance.get('index');
+
+                instance.fire('hide', { index: index });
+            });
+        }
+        else {
             var index = instance.get('index');
 
             instance.fire('hide', { index: index });
-        });
+        }
     }
 }, {
     ATTRS: {
@@ -91,7 +99,19 @@ A.NotifyItem = A.Base.create(NOTIFY_ITEM, A.Widget, [A.WidgetChild, A.WidgetPosi
             value: false
         },
 
-        hideTransition: {
+        hideOn: {
+            valueFn: function() {
+                return [
+                    {
+                        node: this.get(BOUNDING_BOX),
+                        eventName: 'click'
+                    }
+                ];
+            },
+            validator: A.Lang.isArray
+        },
+
+        hideTransition: { 
             value: {
                 opacity: 0
             }
@@ -111,49 +131,64 @@ A.NotifyItem = A.Base.create(NOTIFY_ITEM, A.Widget, [A.WidgetChild, A.WidgetPosi
             value: ''
         },
 
-        textNode: {
-            valueFn: function() {
-                return A.Node.create(TPL_TEXT);
-            }
-        },
-
         timeout: {
             value: 2000
         },
 
+        title: {
+            value: ''
+        },
+
         type: {
-            value: NOTICE
+            validator: function(val) {
+                return (val === ALERT || val === INFO || val === NOTICE);
+            },
+            value: INFO
         }
     }
 });
-var BOUNDING_BOX = 'boundingBox',
+var BODY = 'body',
+    BOUNDING_BOX = 'boundingBox',
+    CENTER = 'center',
+    DIRECTION = 'direction',
     ID = 'id',
+    INDENT = 'indent',
+    MAX_ROWS = 'maxRows',
+    REGION = 'region',
 
-    DIRECTIONS = {
-        down: 'down',
-        left: 'left',
-        right: 'right',
-        up: 'up'
-    },
+    BOTTOM = 'bottom',
+    BOTTOM_LEFT = 'bottom-left',
+    BOTTOM_RIGHT = 'bottom-right',
+    LEFT = 'left',
+    RIGHT = 'right',
+    TOP = 'top',
+    TOP_LEFT = 'top-left',
+    TOP_RIGHT = 'top-right',
 
-    POSITIONS = {
-        'bottom': [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.BL],
-        'bottom-left': [A.WidgetPositionAlign.BL, A.WidgetPositionAlign.BL],
-        'bottom-right': [A.WidgetPositionAlign.BR, A.WidgetPositionAlign.BR],
-        'left': [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TL],
-        'right': [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.TR],
-        'top': [A.WidgetPositionAlign.BL, A.WidgetPositionAlign.TL],
-        'top-left': [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.TL],
-        'top-right': [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TR]
-    };
+    POSITION = 'position',
+
+    POSITIONS = {},
+
+    PX = 'px';
 
 A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent], {
-	regions: null,
+	handles: null,
+    regions: null,
 
 	initializer: function() {
 		var instance = this;
 
+        instance.handles = {};
 		instance.regions = {};
+
+        POSITIONS[BOTTOM] = [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.BL];
+        POSITIONS[BOTTOM_LEFT] = [A.WidgetPositionAlign.BL, A.WidgetPositionAlign.BL];
+        POSITIONS[BOTTOM_RIGHT] = [A.WidgetPositionAlign.BR, A.WidgetPositionAlign.BR];
+        POSITIONS[LEFT] = [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TL];
+        POSITIONS[RIGHT] = [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.TR];
+        POSITIONS[TOP] = [A.WidgetPositionAlign.BL, A.WidgetPositionAlign.TL];
+        POSITIONS[TOP_LEFT] = [A.WidgetPositionAlign.TL, A.WidgetPositionAlign.TL];
+        POSITIONS[TOP_RIGHT] = [A.WidgetPositionAlign.TR, A.WidgetPositionAlign.TR];
 	},
 
 	bindUI: function() {
@@ -166,69 +201,64 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
     _afterAdd: function(event) {
         var instance = this,
         	child = event.child,
-            direction = instance.get('direction'),
+            direction = instance.get(DIRECTION),
         	size = instance.size(),
-            indent = instance.get('indent'),
+            indent = instance.get(INDENT),
         	index = event.index,
-        	alignNode = 'body',
-        	position = POSITIONS[instance.get('position')];
+        	alignNode = BODY,
+        	position = POSITIONS[instance.get(POSITION)];
 
         if (size > 1) {
-       		var maxRows = instance.get('maxRows');
+       		var maxRows = instance.get(MAX_ROWS);
 
         	if ((size % maxRows) == 1) {
       			var previousNode = instance.item(index - maxRows);
 
                 alignNode = previousNode.get(BOUNDING_BOX);
 
-                position = instance._getPosition(indent);
+                position = POSITIONS[indent];
         	}
        		else {
 				var previousNode = instance.item(index - 1);
 
 				alignNode = previousNode.get(BOUNDING_BOX);
 
-                position = instance._getPosition(direction);   
+                position = POSITIONS[direction];   
             }
         }
 
-		if (position == 'center') {
+		if (position == CENTER) {
 		    child.centered(alignNode);
 		}
 		else {
 		    child.align(alignNode, position);
 		}
 
-		child.after(function() {
-			instance.regions[child.get(ID)] = child.get(BOUNDING_BOX).get('region');
+		var handle = child.after(function() {
+			instance.regions[child.get(ID)] = child.get(BOUNDING_BOX).get(REGION);
 		}, child, '_doAlign');
+
+        instance.handles[child.get(ID)] = handle;
     },
 
     _afterHide: function(event) {
     	var instance = this,
-            index = event.index;
+            index = event.index,
+            child = instance.item(index);
 
     	instance._syncRegions(index);
     	instance._moveChildren(index);
 
+        var id = instance.item(index).get(ID);
+
+        var handle = instance.handles[id];
+
+        handle.detach();
+
+        delete instance.handles[id];
+    	delete instance.regions[id];
+
     	instance.remove(index);
-    	delete instance.regions[instance.item(index).get(ID)];
-    },
-
-    _getPosition: function(i) {
-        switch(i) {
-            case DIRECTIONS.down:
-                return POSITIONS.bottom;
-            
-            case DIRECTIONS.up:
-                return POSITIONS.top;
-            
-            case DIRECTIONS.left:
-                return POSITIONS.left;
-
-            case DIRECTIONS.right:
-                return POSITIONS.right;
-        }
     },
 
     _moveChildren: function(index) {
@@ -239,8 +269,7 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
 				return;
 			}
 
-			var previousChild = instance.item(i - 1);
-    		var region = instance.regions[previousChild.get(ID)];
+    		var region = instance.regions[child.get(ID)];
 
 			if (!region) {
 				return;
@@ -249,8 +278,8 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
     		var node = child.get(BOUNDING_BOX);
 
     		node.transition({
-    			top: region.top + 'px',
-    			left: region.left + 'px'
+    			top: region.top + PX,
+    			left: region.left + PX
     		});
     	});
     },
@@ -270,7 +299,7 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
 {
     ATTRS: {
         alignNode: {
-            value: 'body'
+            value: BODY
         },
 
         defaultChildType: {
@@ -280,13 +309,13 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
 
         direction: {
             valueFn: function() {
-                var position = this.get('position');
+                var position = this.get(POSITION);
                 
-                if (position.indexOf('top') === 0) {
-                    return DIRECTIONS.down;
+                if (position.indexOf(TOP) === 0) {
+                    return BOTTOM;
                 }
-                else if (position.indexOf('bottom') === 0) {
-                    return DIRECTIONS.up;
+                else if (position.indexOf(BOTTOM) === 0) {
+                    return TOP;
                 }
 
                 return undefined;
@@ -295,13 +324,13 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
 
         indent: {
             valueFn: function() {
-                var position = this.get('position');
+                var position = this.get(POSITION);
                 
-                if (position.indexOf('right') !== -1) {
-                    return DIRECTIONS.left;
+                if (position.indexOf(RIGHT) !== -1) {
+                    return LEFT;
                 }
-                else if (position.indexOf('left') !== -1) {
-                    return DIRECTIONS.right;
+                else if (position.indexOf(LEFT) !== -1) {
+                    return RIGHT;
                 }
 
                 return undefined;
@@ -313,9 +342,9 @@ A.NotifyContainer = A.Base.create('notify-container', A.Widget, [A.WidgetParent]
         },
 
         position: {
-            value: 'top-left'
+            value: TOP_LEFT
         }
     }
 });
 
-}, '@VERSION@' ,{skinnable:true, requires:['aui-base','transition','widget','widget-child','widget-parent','widget-position','widget-position-align']});
+}, '@VERSION@' ,{skinnable:true, requires:['aui-base','transition','widget','widget-autohide','widget-child','widget-parent','widget-position','widget-position-align','widget-stdmod']});
