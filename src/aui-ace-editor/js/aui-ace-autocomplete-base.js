3f31f0a337eba8701b8513d82cf7218cc36ca47a
Iliyan Peychev <iliyan.peychev@gmail.com>
2013-03-16 14:08:03 +0100

    AUI-848 - Remove Ternary Search Tree from ACE AutoComplete

--- src/aui-ace-editor/js/aui-ace-autocomplete-base.js
+++ src/aui-ace-editor/js/aui-ace-autocomplete-base.js
@@ -32,6 +32,9 @@ Base.prototype = {
         if (processor && !processor.get(HOST)) {
             processor.set(HOST, instance);
         }
+
+        instance._onResultsErrorFn = A.bind(instance._onResultsError, instance);
+        instance._onResultsSuccessFn = A.bind(instance._onResultsSuccess, instance);
     },
 
     _addSuggestion: function(content) {
@@ -44,10 +47,23 @@ Base.prototype = {
         var data = instance.get(PROCESSOR).getSuggestion(instance._matchParams.match, content);
 
         if (this.get(FILL_MODE) === Base.FILL_MODE_OVERWRITE) {
-            editor.removeWordLeft();
-        }
+            var matchParams = instance._matchParams;
 
-        editor.insert(data);
+            var startRow = matchParams.row;
+
+            var startColumn = matchParams.column - matchParams.match.content.length;
+
+            var cursorPosition = editor.getCursorPosition();
+
+            var Range = require('ace/range').Range;
+
+            var overwriteRange = new Range(startRow, startColumn, cursorPosition.row, cursorPosition.column);
+
+            editor.getSession().replace(overwriteRange, data);
+        }
+        else {
+            editor.insert(data);
+        }
 
         editor.focus();
 
@@ -70,7 +86,9 @@ Base.prototype = {
 
         var editor = instance._getEditor();
 
-        editor.on('change', A.bind(instance._onEditorChange, instance));
+        instance._onChangeFn = A.bind(instance._onEditorChange, instance);
+
+        editor.on('change', instance._onChangeFn);
 
         editor.commands.addCommand(
             {
@@ -128,6 +146,8 @@ Base.prototype = {
         var editor = instance._getEditor();
 
         editor.commands.removeCommand('showAutoComplete');
+
+        editor.removeListener('change', instance._onChangeFn);
 
         editor.getSelection().removeListener('changeCursor', instance._onEditorChangeCursorFn);
 
@@ -247,7 +267,7 @@ Base.prototype = {
                 row: row
             };
 
-            processor.getResults(match, A.bind(instance._onResultsSuccess, instance), A.bind(instance._onResultsError, instance));
+            processor.getResults(match, instance._onResultsSuccessFn, instance._onResultsErrorFn);
         }
 
         instance.fire(
