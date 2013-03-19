e9e214cbe6170720c83a0c9231234de2f46c123d
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-03-19 18:14:24 +0100

    AUI-858 - Implement AutoComplete for Velocity language in ACE Editor

--- src/aui-ace-editor/js/aui-ace-autocomplete-freemarker.js
+++ src/aui-ace-editor/js/aui-ace-autocomplete-freemarker.js
@@ -1,219 +1,112 @@
 var Lang = A.Lang,
-    AArray = A.Array,
-    AObject = A.Object,
 
-    DIRECTIVES = [
-        'assign',
-        'attempt',
-        'break',
-        'case',
-        'compress',
-        'default',
-        'else',
-        'elseif',
-        'escape',
-        'fallback',
-        'flush',
-        'ftl',
-        'function',
-        'global',
-        'if',
-        'import',
-        'include',
-        'list',
-        'local',
-        'lt',
-        'macro',
-        'nested',
-        'noescape',
-        'nt',
-        'recover',
-        'recurse',
-        'return',
-        'rt',
-        'setting',
-        'stop',
-        'switch',
-        't',
-        'visit'
-    ],
+	Base = A.AceEditor.AutoCompleteBase,
 
-    Base = A.AceEditor.AutoCompleteBase,
+	MATCH_DIRECTIVES = 0,
+	MATCH_VARIABLES = 1,
 
-    MATCH_DIRECTIVES = 0,
-    MATCH_VARIABLES = 1,
-
-    REGEX_DIRECTIVES = /<#[\w.]*>?$/,
-    REGEX_VARIABLES = /\$\{[\w., ()"]*\}?$/,
-
-    STATUS_ERROR = -1,
-    STATUS_SUCCESS = 0,
-
-    ALL =  'all',
-    DOT = '.',
-    HOST = 'host',
-    STR_EMPTY = '',
-    STR_RESPONSE_DATA = 'responseData',
-    VARIABLES = 'variables',
-
-    NAME = 'aui-ace-autocomplete-freemarker';
+	NAME = 'aui-ace-autocomplete-freemarker';
 
 var Freemarker = A.Component.create({
-    NAME: NAME,
+	NAME: NAME,
 
-    NS: NAME,
+	NS: NAME,
 
-    ATTRS: {
-        host: {
-            validator: Lang.isObject
-        },
+	ATTRS: {
+		directives: {
+			validator: Lang.isArray,
+			value: [
+				'assign',
+				'attempt',
+				'break',
+				'case',
+				'compress',
+				'default',
+				'else',
+				'elseif',
+				'escape',
+				'fallback',
+				'flush',
+				'ftl',
+				'function',
+				'global',
+				'if',
+				'import',
+				'include',
+				'list',
+				'local',
+				'lt',
+				'macro',
+				'nested',
+				'noescape',
+				'nt',
+				'recover',
+				'recurse',
+				'return',
+				'rt',
+				'setting',
+				'stop',
+				'switch',
+				't',
+				'visit'
+			]
+		},
 
-        variables: {
-            validator: Lang.isObject
-        }
-    },
+		directivesMatcher: {
+			setter: '_setRegexValue',
+			value: /<#[\w]*[^<#]*$/
+		},
 
-    EXTENDS: A.Base,
+		host: {
+			validator: Lang.isObject
+		},
 
-    prototype: {
-        getMatch: function(content) {
-            var instance = this;
+		variables: {
+			validator: Lang.isObject
+		},
 
-            var match;
+		variablesMatcher: {
+			setter: '_setRegexValue',
+			value: /\${[\w., ()"]*(?:[^$]|\\\$)*$/
+		}
+	},
 
-            var matchIndex;
+	EXTENDS: A.AceEditor.TemplateProcessor,
 
-            if ((matchIndex = content.lastIndexOf('<')) >= 0) {
-                content = content.substring(matchIndex);
+	prototype: {
+		getMatch: function(content) {
+			var instance = this;
 
-                if (REGEX_DIRECTIVES.test(content)) {
-                    match = {
-                        content: content.substring(2),
-                        start: matchIndex,
-                        type: MATCH_DIRECTIVES
-                    };
-                }
-            }
-            else if ((matchIndex = content.lastIndexOf('$')) >= 0) {
-                content = content.substring(matchIndex);
+			var match;
 
-                if (REGEX_VARIABLES.test(content)) {
-                    match = {
-                        content: content.substring(2),
-                        start: matchIndex,
-                        type: MATCH_VARIABLES
-                    };
-                }
-            }
+			var matchIndex;
 
-            return match;
-        },
+			if ((matchIndex = content.lastIndexOf('<')) >= 0) {
+				content = content.substring(matchIndex);
 
-        getResults: function(match, callbackSuccess, callbackError) {
-            var instance = this;
+				if (instance.get('directivesMatcher').test(content)) {
+					match = {
+						content: content.substring(2),
+						start: matchIndex,
+						type: MATCH_DIRECTIVES
+					};
+				}
+			}
+			else if ((matchIndex = content.lastIndexOf('$')) >= 0) {
+				content = content.substring(matchIndex);
 
-            var type = match.type;
+				if (instance.get('variablesMatcher').test(content)) {
+					match = {
+						content: content.substring(2),
+						start: matchIndex,
+						type: MATCH_VARIABLES
+					};
+				}
+			}
 
-            if (type === MATCH_DIRECTIVES) {
-                var matchDirectives = DIRECTIVES;
-
-                var content = match.content.toLowerCase();
-
-                if (content.length) {
-                    var host = instance.get(HOST);
-
-                    matchDirectives = host._filterResults(content, matchDirectives);
-                }
-
-                callbackSuccess(matchDirectives);
-            }
-            else if (type === MATCH_VARIABLES) {
-                var matches = instance._getVariableMatches(match.content);
-
-                callbackSuccess(matches);
-            }
-        },
-
-        getSuggestion: function(match, selectedSuggestion) {
-            var instance = this;
-
-            var result = selectedSuggestion || STR_EMPTY;
-
-            if (selectedSuggestion) {
-                var fillMode = instance.get(HOST).get('fillMode');
-
-                var type = match.type;
-
-                var variables;
-
-                var lastEntry;
-
-                if (fillMode === Base.FILL_MODE_INSERT) {
-                    if (type === MATCH_DIRECTIVES) {
-                        if (match.content && selectedSuggestion.indexOf(match.content) === 0) {
-                            result = selectedSuggestion.substring(match.content.length);
-                        }
-                    }
-                    else if (type === MATCH_VARIABLES) {
-                        variables = match.content.split(DOT);
-
-                        lastEntry = variables[variables.length - 1];
-
-                        if (lastEntry && selectedSuggestion.indexOf(lastEntry) === 0) {
-                            result = selectedSuggestion.substring(lastEntry.length);
-                        }
-                    }
-                }
-                else if (type === MATCH_VARIABLES) {
-                    variables = match.content.split(DOT);
-
-                    variables[variables.length - 1] = selectedSuggestion;
-
-                    result = variables.join(DOT);
-                }
-            }
-
-            return result;
-        },
-
-        _getVariableMatches: function(content) {
-            var instance = this;
-
-            var variables = content.split(DOT);
-
-            var variableCache = instance.get(VARIABLES);
-
-            var lastEntry = variables[variables.length - 1];
-
-            variables.length -= 1;
-
-            var variable;
-
-            if (variables.length > 0) {
-                for (var i = 0; i < variables.length; i++) {
-                    variable = variables[i];
-
-                    if (Lang.isObject(variableCache)) {
-                        variableCache = variableCache[variable];
-                    }
-                }
-            }
-
-            var matches = [];
-
-            lastEntry = lastEntry.toLowerCase();
-
-            if (Lang.isObject(variableCache)) {
-                var host = instance.get(HOST);
-
-                matches = host._filterResults(lastEntry, AObject.keys(variableCache));
-            }
-
-            return matches;
-        }
-    }
+			return match;
+		}
+	}
 });
-
-Freemarker.DIRECTIVES = DIRECTIVES;
 
 A.AceEditor.AutoCompleteFreemarker = Freemarker;
