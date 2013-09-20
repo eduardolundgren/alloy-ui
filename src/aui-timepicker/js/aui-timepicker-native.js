496f104ba2ed89c91089b51c84607ea4beea0068
Zeno Rocha <zno.rocha@gmail.com>
2013-09-20 12:02:08 -0300

    Add API Docs placeholder for TimePickerNative

--- src/aui-timepicker/js/aui-timepicker-native.js
+++ src/aui-timepicker/js/aui-timepicker-native.js
@@ -1,15 +1,52 @@
+/**
+ * The TimePicker Component
+ *
+ * @module aui-timepicker
+ * @submodule aui-timepicker-native
+ */
+
 var Lang = A.Lang,
 
     TIME = 'time';
 
+/**
+ * A base class for TimePickerNativeBase.
+ *
+ * @class A.TimePickerNativeBase
+ * @param config {Object} Object literal specifying widget configuration properties.
+ * @constructor
+ */
 function TimePickerNativeBase() {}
 
+/**
+ * Static property used to define the default attribute
+ * configuration for the TimePickerNativeBase.
+ *
+ * @property TimePickerNativeBase.ATTRS
+ * @type Object
+ * @static
+ */
 TimePickerNativeBase.ATTRS = {
+
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @attribute nativeMask
+     * @default '%H:%M'
+     * @type String
+     */
     nativeMask: {
         validator: Lang.isString,
         value: '%H:%M'
     },
 
+    /**
+     * TODO. Wanna help? Please send a Pull Request.
+     *
+     * @attribute nativeType
+     * @default 'time'
+     * @type String
+     */
     nativeType: {
         validator: Lang.isString,
         value: TIME
@@ -18,4 +55,13 @@ TimePickerNativeBase.ATTRS = {
 
 A.TimePickerNativeBase = TimePickerNativeBase;
 
+/**
+ * A base class for TimePickerNative.
+ *
+ * @class A.TimePickerNative
+ * @extends A.Base
+ * @uses A.DatePickerDelegate, A.DatePickerNativeBase, A.TimePickerNativeBase
+ * @param config {Object} Object literal specifying widget configuration properties.
+ * @constructor
+ */
 A.TimePickerNative = A.Base.create('timepicker-native', A.Base, [A.DatePickerDelegate, A.DatePickerNativeBase, A.TimePickerNativeBase]);
