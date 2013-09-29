6336cadc82fa9a3d85425cf7646e8b49d5b7e45a
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-09-29 22:57:14 +0200

    AUI-985 - Fix DatePicker hide on Firefox
    On Firefox, focusin and focusout are not yet supported.
    Replacing them with focus and blur and getting the target via
    event.target instead event.currentTarget resolves the issue.
    This works on all the other A-grade browsers too.

--- src/aui-datepicker/HISTORY.md
+++ src/aui-datepicker/HISTORY.md
@@ -1,2 +1,8 @@
 aui-datepicker
 ========
+
+
+@VERSION@
+------
+
+	* #AUI-985 - Fix DatePicker behavior on focus/blur in Firefox
--- src/aui-datepicker/tests/unit/js/tests.js
+++ src/aui-datepicker/tests/unit/js/tests.js
@@ -1,16 +1,58 @@
 YUI.add('module-tests', function(Y) {
 
-    var suite = new Y.Test.Suite('aui-datepicker');
+    //--------------------------------------------------------------------------
+    // DatePicker Tests
+    //--------------------------------------------------------------------------
+
+    var suite = new Y.Test.Suite('aui-datepicker'),
+        datePicker;
+
+    //--------------------------------------------------------------------------
+    // Test Case for DatePickerDelegate events
+    //--------------------------------------------------------------------------
 
     suite.add(new Y.Test.Case({
-        name: 'Automated Tests',
-        'test is empty': function() {
-            Y.Assert.fail('No Tests Provided For This Module');
+
+        name: 'DatePicker focus/blur tests',
+
+        /**
+         * @tests AUI-985
+         */
+        'assert DatePicker shows and hides on focus/blur events': function() {
+            var instance = this;
+
+            datePicker = new Y.TimePicker({
+                trigger: '#delegate input',
+                on: {
+                    selectionChange: function(event) {
+                        console.log(event.newSelection);
+                    }
+                }
+            });
+
+            instance._showDatePicker('#pickerInput1');
+
+            Y.Test.Assert.isNull(Y.one('.timepicker-popover.popover-hidden'),
+                'DatePicker should be visible now');
+
+            Y.one('#outside').focus();
+            Y.one('#outside').simulate('click');
+
+            Y.Test.Assert.isNotNull(Y.one('.timepicker-popover.popover-hidden'),
+                'DatePicker should be hidden now');
+        },
+
+        _showDatePicker: function(trigger) {
+            trigger = Y.one(trigger);
+
+            trigger.simulate('mousedown');
+            trigger.focus();
+            trigger.simulate('click');
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 }, '', {
-    requires: ['test']
+    requires: ['aui-timepicker', 'aui-timepicker-native', 'event-custom-base', 'node-event-simulate', 'test']
 });
