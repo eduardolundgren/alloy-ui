d9c616a98a7e877ccccc4c8b46b28f1c6f788b06
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-03-09 19:26:42 -0300

    Yogi loader regenerate

--- src/aui-base/js/aui-loader.js
+++ src/aui-base/js/aui-loader.js
@@ -832,6 +832,16 @@ Y.mix(YUI.Env[Y.version].modules, {
             "aui-component"
         ]
     },
+    "aui-surface": {
+        "requires": [
+            "base-build",
+            "node-style",
+            "pjax-base",
+            "promise",
+            "router",
+            "aui-set"
+        ]
+    },
     "aui-tabview": {
         "requires": [
             "selector-css3",
@@ -1045,4 +1055,4 @@ Y.mix(YUI.Env[Y.version].modules, {
         ]
     }
 });
-YUI.Env[Y.version].md5 = '8d2112cfec7c545d4f53ed15310ca2ed';
+YUI.Env[Y.version].md5 = 'e8cdfca700308043481f507690524b69';
