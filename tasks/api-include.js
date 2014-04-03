05d8ee5fecef39e9d76e0e1a7faa55609ad806eb
Henrique Vicente <henriquevicente@gmail.com>
2014-04-03 14:41:43 -0300

    AUI-1209 Simplify the overwrite of grunt config options by the tasks with the grunt-cli-config plugin, simplifying the code
    Signed-off-by: Henrique Vicente <henriquevicente@gmail.com>

--- tasks/api-include.js
+++ tasks/api-include.js
@@ -9,7 +9,6 @@ var fs = require('fs');
 var path = require('path');
 
 // -- Globals ------------------------------------------------------------------
-var ROOT = process.cwd();
 
 // -- Task ---------------------------------------------------------------------
 module.exports = function(grunt) {
@@ -18,10 +17,9 @@ module.exports = function(grunt) {
         var files = this.files[0].src;
         var target = this.target;
 
+        grunt.applyCliConfig(TASK.name, target);
+
         async.series([
-            function(mainCallback) {
-                exports._setGruntConfig(mainCallback, target);
-            },
             function(mainCallback) {
                 exports._replaceInclude(mainCallback, target, files);
             }],
@@ -35,36 +33,6 @@ module.exports = function(grunt) {
             }
         );
     });
-
-    exports._setGruntConfig = function(mainCallback, target) {
-        var options = grunt.option.flags();
-
-        options.forEach(function(option) {
-            var key;
-            var value;
-            var valueIndex;
-
-            // Normalize option
-            option = option.replace(/^--(no-)?/, '');
-
-            valueIndex = option.lastIndexOf('=');
-
-            // String parameter
-            if (valueIndex !== -1) {
-                key = option.substring(0, valueIndex);
-                value = option.substring(valueIndex + 1);
-            }
-            // Boolean parameter
-            else {
-                key = option;
-                value = grunt.option(key);
-            }
-
-            grunt.config([TASK.name, target, key], value);
-        });
-
-        mainCallback();
-    };
 
     exports._replaceInclude = function(mainCallback, target, files) {
         files.forEach(function(file) {
