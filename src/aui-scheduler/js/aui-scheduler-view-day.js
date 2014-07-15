5b6b93ed17dafb0947e043b5a471b8ed5b02d410
Maira Bello <maira.araujo@liferay.com>
2014-07-15 15:20:30 -0300

    AUI-1531 Scroll to current time when opening scheduler

--- src/aui-scheduler/js/aui-scheduler-view-day.js
+++ src/aui-scheduler/js/aui-scheduler-view-day.js
@@ -60,6 +60,8 @@ var Lang = A.Lang,
     CSS_SCHEDULER_TODAY = getCN('scheduler', 'today'),
     CSS_SCHEDULER_TODAY_HD = getCN('scheduler', 'today', 'hd'),
 
+    CSS_SCHEDULER_VIEW_SCROLLABLE = getCN('scheduler-view', 'scrollable'),
+
     CSS_SCHEDULER_VIEW_DAY_COLDATA = getCN('scheduler-view', 'coldata'),
     CSS_SCHEDULER_VIEW_DAY_COLGRID = getCN('scheduler-view', 'colgrid'),
     CSS_SCHEDULER_VIEW_DAY_GRID = getCN('scheduler-view', 'grid'),
@@ -822,6 +824,20 @@ var SchedulerDayView = A.Component.create({
             });
 
             instance.syncHeaderViewUI();
+        },
+
+        /**
+         * Scrolls to given date.
+         *
+         * @method scrollToDate
+         * @param {Date} date The date to scroll to
+         */
+        scrollToDate: function(date) {
+            var scrollNode = this.get('boundingBox').one('.' + CSS_SCHEDULER_VIEW_SCROLLABLE);
+
+            if (this.get('scrollable') && scrollNode) {
+                scrollNode.set('scrollTop', this.calculateTop(date));
+            }
         },
 
         /**
