4bf4d003440168538e2601fb9ac160161325588b
Jonathan Mak <jonathan.mak@liferay.com>
2014-08-22 20:06:24 -0700

    AUI-1555 - Source formatting

--- src/aui-base/tests/unit/js/tests.js
+++ src/aui-base/tests/unit/js/tests.js
@@ -1,6 +1,18 @@
 YUI.add('aui-base-tests', function(Y) {
 
-    var escapedEntities = ['&amp;', '&lt;', '&gt;', '&#034;', '&#039;', '&#047;', '&#096;'],
+    var endsWithStrings = [
+            'lorem-ipsum',
+            'lorem ipsum',
+            'loremipsumdolor',
+            'lorem'
+        ],
+        endsWithStringsSuffixes = [
+            'ipsum',
+            ' ipsum',
+            'dolor',
+            'm'
+        ],
+        escapedEntities = ['&amp;', '&lt;', '&gt;', '&#034;', '&#039;', '&#047;', '&#096;'],
         numbersToPad = [1, 10, 2.5, 6.789, 123.4, 3000.3102, .5, .10001, 500000.0],
         symbolEntities = ['&','<','>','"','\'','/','`'],
         uncamelizedStrings = [
@@ -9,24 +21,6 @@ YUI.add('aui-base-tests', function(Y) {
             'Lorem-Ipsum-doLOR. sit-amet +1',
             'lorem-ipsum-dolor-sit-amet, LOREM-ipsum-D&OLOR',
             'Lorem-ipsum-dolor-sit-amet. lorem-ipsum-dolor-sit-amet, lorem-Ipsum-Dolor-Sit-Amet',
-        ],
-        endsWithStrings = [
-            'novemnonagintillion ',
-            'flibberti gibbet',
-            'balderdash456',
-            'codswallop'
-        ],
-        endsWithStringsSuffixes = [
-            'gintillion ',
-            ' gibbet',
-            '456',
-            'codswallop'
-        ],
-        endsWithStringsWrongSuffixes = [
-            'gintillion',
-            'bet ',
-            'dash',
-            'cods'
         ];
 
     var Assert = Y.Assert,
@@ -130,10 +124,6 @@ YUI.add('aui-base-tests', function(Y) {
         'should check for ending suffix correctly': function() {
             for (var i = 0; i < endsWithStrings.length; i++) {
                 Assert.isTrue(Y.Lang.String.endsWith(endsWithStrings[i], endsWithStringsSuffixes[i]));
-            }
-
-            for (var j = 0; j < endsWithStrings.length; j++) {
-                Assert.isTrue(!Y.Lang.String.endsWith(endsWithStrings[j], endsWithStringsWrongSuffixes[j]));
             }
         }
     }));
